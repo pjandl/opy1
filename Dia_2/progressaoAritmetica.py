c06dabf0e48d16a9716d3c5f970d5ef79277c118
PJandl <pjandl@gmail.com>
2020-09-29 16:04:29 -0300

    Add files via upload
    Exemplo incompleto, agora revisado e funcional.

--- Dia_2/progressaoAritmetica.py
+++ Dia_2/progressaoAritmetica.py
@@ -1,7 +1,28 @@
-# Testa se três inteiros formam uma progressão aritmética
-print ('Progressão Aritmética')
+# Testa se três inteiros formam uma progressão aritmética ou geométrica
+print ('Progressão Aritmética ou Geométrica')
 
-a = int(input('Digite um inteiro a: '))
-b = int(input('Digite um inteiro b: '))
-c = int(input('Digite um inteiro c: '))
+a = float(input('Digite um inteiro a: '))
+b = float(input('Digite um inteiro b: '))
+c = float(input('Digite um inteiro c: '))
+
+# se a distância/diferença entre a e b é a mesma que entre b e c,
+# temos um PA (progressão aritmética)
+if b-a == c-b:
+    print('a, b e c formam uma Progressão Aritmética de razão {:.2f}.'
+          .format(b-a))
+else:
+    print('a, b e c NÃO formam uma Progressão Aritmética.')
+    
+# se a ou b tiverem valor zero, não é possível que seja
+# uma progressão geométrica, então finaliza o programa
+if a==0 or b==0: exit()
+
+# se a proporção/razao entre a e b é a mesma que entre b e c,
+# temos um PG (progressão geométrica)
+if b/a == c/b:
+    print('a, b e c formam uma Progressão Geométrica de razão {:f}.'
+          .format(b/a))
+else:
+    print('a, b e c NÃO formam uma Progressão Geométrica.')
+    
 
